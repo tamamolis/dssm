3076518f71a70b8936b2088398f7130bc68ecb23
Katherine <tamamo.lis@yandex.ru>
2018-06-13 15:00:59 +0300

    Add files via upload

--- src/nnModel/dssm.py
+++ src/nnModel/dssm.py
@@ -1,11 +1,13 @@
-#coding=utf-8
+# coding=utf-8
+from __future__ import print_function
 import os
 import sys
 import numpy as np
 import tensorflow as tf
-from importlib import reload
+# from importlib import reload
+from preprocess import preprocess
 
-reload(sys)
+# reload(sys)
 # sys.setdefaultencoding('utf8')
 
 flags = tf.app.flags
@@ -15,12 +17,14 @@ flags.DEFINE_integer('embedding_dim', 200, 'the dim of the fusion of multi word2
 flags.DEFINE_integer('sentence_length', 20, 'sentence max word')
 flags.DEFINE_boolean('use_gpu', False, 'use gpu or not')
 flags.DEFINE_string('model_version', 'v1', 'model version')
-flags.DEFINE_boolean('fine_tune', False, 'enable word embedding fine tune')
-flags.DEFINE_integer('vocab_size', 2278, 'the size of the vocab')
+flags.DEFINE_boolean('fine_tune', True, 'enable word embedding fine tune')
+flags.DEFINE_integer('vocab_size', 1952, 'the size of the vocab') #было 2278
+
 
 class Model(object):
 
-    def __init__(self, sample = 11, sample_nums = 200, lr = 0.1, lrdf = 0.95, l1 = 1000, l2 = 240, l3 = 400, cks = [2,3,4], ckn = [128, 64, 64]):
+    def __init__(self, sample=11, sample_nums=200, lr=0.1, lrdf=0.95, l1=1000, l2=240, l3=400, cks=[2, 3, 4],
+                 ckn=[128, 64, 64]):
         self.SAMPLE = sample
         self.BS = self.SAMPLE * sample_nums
         self.LEARNING_RATE = lr
@@ -46,9 +50,11 @@ class Model(object):
 
             with tf.name_scope('w2v'):
                 if FLAGS.fine_tune:
-                    self.words = tf.Variable(self.__load_w2v('../../model/vec.txt', FLAGS.embedding_dim), dtype=tf.float32, name='words')
+                    self.words = tf.Variable(self.__load_w2v('../../model/vec.txt', FLAGS.embedding_dim),
+                                             dtype=tf.float32, name='words')
                 else:
-                    self.words = tf.Variable(tf.random_uniform([FLAGS.vocab_size, FLAGS.embedding_dim]), dtype=tf.float32, name='words')
+                    self.words = tf.Variable(tf.random_uniform([FLAGS.vocab_size, FLAGS.embedding_dim]),
+                                             dtype=tf.float32, name='words')
 
                 self.query_words = tf.nn.embedding_lookup(self.words, self.query)
                 self.doc_words = tf.nn.embedding_lookup(self.words, self.doc)
@@ -67,28 +73,35 @@ class Model(object):
                 self.doc_pool_list = []
                 for i, size in enumerate(self.conv_kernel_size):
                     # conv kernel size = i
-                    self.wc[size] = tf.Variable(tf.random_normal([size, FLAGS.embedding_dim, 1, self.conv_kernel_number[i]]),
-                                           'wc' + str(size))
+                    self.wc[size] = tf.Variable(
+                        tf.random_normal([size, FLAGS.embedding_dim, 1, self.conv_kernel_number[i]]),
+                        'wc' + str(size))
                     self.bc[size] = tf.Variable(tf.random_normal([self.conv_kernel_number[i]]), 'bc' + str(size))
 
-                    self. query_conv[size] = self.__conv2d('conv' + str(size), self.query_words_out, self.wc[size], self.bc[size])
-                    self.query_pool[size] = self.__full_max_pool('pool' + str(size), self.query_conv[size], [0, 3, 2, 1])
+                    self.query_conv[size] = self.__conv2d('conv' + str(size), self.query_words_out, self.wc[size],
+                                                          self.bc[size])
+                    self.query_pool[size] = self.__full_max_pool('pool' + str(size), self.query_conv[size],
+                                                                 [0, 3, 2, 1])
                     self.query_pool_list.append(self.query_pool[size])
 
-                    self.doc_conv[size] = self.__conv2d('conv' + str(size), self.doc_words_out, self.wc[size], self.bc[size])
+                    self.doc_conv[size] = self.__conv2d('conv' + str(size), self.doc_words_out, self.wc[size],
+                                                        self.bc[size])
                     self.doc_pool[size] = self.__full_max_pool('pool' + str(size), self.doc_conv[size], [0, 3, 2, 1])
                     self.doc_pool_list.append(self.doc_pool[size])
 
                 self.query_pool_merge = tf.concat(self.query_pool_list, 3)
-                self.query_conv_out = tf.nn.l2_normalize(tf.nn.relu(tf.reshape(self.query_pool_merge, [-1, sum(self.conv_kernel_number)])), 1)
+                self.query_conv_out = tf.nn.l2_normalize(
+                    tf.nn.relu(tf.reshape(self.query_pool_merge, [-1, sum(self.conv_kernel_number)])), 1)
 
                 self.doc_pool_merge = tf.concat(self.doc_pool_list, 3)
-                self.doc_conv_out = tf.nn.l2_normalize(tf.nn.relu(tf.reshape(self.doc_pool_merge, [-1, sum(self.conv_kernel_number)])),
-                                                  1)
+                self.doc_conv_out = tf.nn.l2_normalize(
+                    tf.nn.relu(tf.reshape(self.doc_pool_merge, [-1, sum(self.conv_kernel_number)])),
+                    1)
 
             with tf.name_scope('dense_layer_1'):
                 self.l1_par_range = np.sqrt(6.0 / (sum(self.conv_kernel_number) + self.L1_N))
-                self.wd1 = tf.Variable(tf.random_uniform([sum(self.conv_kernel_number), self.L1_N], -self.l1_par_range, self.l1_par_range))
+                self.wd1 = tf.Variable(
+                    tf.random_uniform([sum(self.conv_kernel_number), self.L1_N], -self.l1_par_range, self.l1_par_range))
                 self.bd1 = tf.Variable(tf.random_uniform([self.L1_N], -self.l1_par_range, self.l1_par_range))
 
                 self.query_l1 = tf.matmul(self.query_conv_out, self.wd1) + self.bd1
@@ -143,11 +156,12 @@ class Model(object):
                 self.label_y = tf.argmax(self.label, 1)
                 self.pred = tf.equal(self.pred_y, self.label_y)
                 self.accuracy = tf.reduce_mean(tf.cast(self.pred, tf.float32))
-                self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.out, labels=self.label))
+                self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.out, labels=self.label))
 
             with tf.name_scope('train'):
                 self.learning_rate = tf.Variable(float(self.LEARNING_RATE), trainable=False)
-                self.learning_rate_decay_op = self.learning_rate.assign(self.learning_rate * self.LEARNING_RATE_DECAY_FACTOR)
+                self.learning_rate_decay_op = self.learning_rate.assign(
+                    self.learning_rate * self.LEARNING_RATE_DECAY_FACTOR)
                 #        train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
                 self.train_step = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss)
 
@@ -159,13 +173,14 @@ class Model(object):
         fp = open(path, "r")
         line = fp.readline().strip()
         ss = line.split(" ")
-        total = int(ss[0])
+        total = int(ss[0]) - 1
         dim = int(ss[1])
         assert (dim == expectDim)
         ws = []
         for t in range(total):
             line = fp.readline().rstrip()
             ss = line.split(" ")[1:]
+            print('assert: ', t, len(ss), dim, total)
             assert (len(ss) == dim)
             vals = []
             for i in range(0, dim):
@@ -173,6 +188,7 @@ class Model(object):
                 vals.append(fv)
             ws.append(vals)
         fp.close()
+        print('assert2: ', len(ws), total)
         assert total == len(ws)
         print ("w2v size : " + str(total))
         return np.asarray(ws, dtype=np.float32)
