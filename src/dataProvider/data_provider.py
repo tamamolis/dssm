fc8571c4dc1f56c6d0d918bd4637d316dbdb8b24
Katherine <tamamo.lis@yandex.ru>
2018-06-25 12:06:36 +0300

    Add files via upload

--- src/dataProvider/data_provider.py
+++ src/dataProvider/data_provider.py
@@ -1,6 +1,8 @@
-#coding=utf-8
+# coding=utf-8
+from __future__ import print_function
 import random
 import numpy as np
+import gensim
 
 train_query_path = '../../dataset/train_query.txt'
 train_doc_path = '../../dataset/train_doc.txt'
@@ -12,24 +14,29 @@ test_query_path = '../../dataset/test_query.txt'
 test_doc_path = '../../dataset/test_doc.txt'
 test_label_path = '../../dataset/test_label.txt'
 
+
 def load_train_dataset():
     return load_dataset(train_query_path, train_doc_path, train_label_path)
+
 
 def load_valid_dataset():
     return load_dataset(valid_query_path, valid_doc_path, valid_label_path)
 
+
 def load_test_dataset():
     return load_dataset(test_query_path, test_doc_path, test_label_path)
 
+
 def load_dataset(query_path, doc_path, label_path):
-#    global train_query, train_doc, train_label, test_query, test_doc, test_label
-    query_data = get_onehot_vec(query_path, sentence_length = 20)
+    #    global train_query, train_doc, train_label, test_query, test_doc, test_label
+    query_data = get_onehot_vec(query_path, sentence_length=20)
     doc_data = get_onehot_vec(doc_path)
     label_data = get_label(label_path)
-    print (query_data.shape, doc_data.shape, label_data.shape)
+    print(query_data.shape, doc_data.shape, label_data.shape)
     assert query_data.shape == doc_data.shape
     assert query_data.shape[0] == label_data.shape[0]
     return query_data, doc_data, label_data
+
 
 def get_onehot_vec(filepath, sentence_length=20):
     global vocab
@@ -39,7 +46,7 @@ def get_onehot_vec(filepath, sentence_length=20):
         temp = []
         row = row.strip().split()
         if len(row) == 0:
-            print ("sentence has no word")
+            print("sentence has no word")
             continue
         for word in row:
             if word in vocab:
@@ -47,10 +54,31 @@ def get_onehot_vec(filepath, sentence_length=20):
         if len(temp) > sentence_length:
             temp = temp[:sentence_length]
         elif len(temp) < sentence_length:
-            temp = temp + [0]*(sentence_length-len(temp))
+            temp = temp + [0] * (sentence_length - len(temp))
         data_set.append(temp)
     fr.close()
     return np.asarray(data_set, dtype=np.int32)
+
+
+def get_w2v(filepath_test, sentence_length=20):
+    global vocab
+
+    # filepath = "/Users/kate/PycharmProjects/dssm/dataset/GoogleNews-vectors-negative300.bin.gz"
+    # model = gensim.models.KeyedVectors.load_word2vec_format(filepath, binary=False)
+
+    sentences = gensim.models.word2vec.PathLineSentences('/Users/kate/PycharmProjects/dssm/raw_data/w2v_corpus.txt')
+    model = gensim.models.Word2Vec(sentences, min_count=1, size=200)
+
+    fr = open(filepath_test)
+    for row in fr.readlines():
+        temp = []
+        row = row.strip().split()
+        for word in row:
+            if word in vocab:
+                temp.append(model[word])
+
+    return np.asarray(temp)
+
 
 def get_label(filepath):
     label = []
@@ -66,6 +94,7 @@ def get_label(filepath):
     fr.close()
     return np.asarray(label, dtype=np.int32)
 
+
 def load_vocab():
     vocab_path = '../../model/vocab.txt'
     vocab = {}
@@ -79,9 +108,10 @@ def load_vocab():
         index += 1
     fr.close()
     return vocab
-    fw.close()
+    # fw.close()
+
 
 vocab = load_vocab()
 
-if __name__=="__main__":
+if __name__ == "__main__":
     load_vocab()
